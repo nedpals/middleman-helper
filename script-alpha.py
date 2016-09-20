5b3be03358526fd987ce8b4e453211a859449290
Ned Palacios <npdoesmc@gmail.com>
2016-09-20 19:27:32 +0800

    Updated script to 0.2
    Removed testing command and adds working article creation

--- script-alpha.py
+++ script-alpha.py
@@ -1,14 +1,18 @@
 import io
+import os
 import datetime
 import re
 
 # Middleman Helper Init
 init = 1
 
+# Does site have blog dir?
+blog_dir = 1
+
 # Main console
-print "Middleman Helper v0.1-alpha".center(60, ' ')
+print "Middleman Helper v0.2-alpha".center(60, ' ')
 print "by Ned Palacios".center(60,' ')
-print "https://gitlab.com/nedpals/middleman-helper".center(60,' ')
+print "https://github.com/nedpals/middleman-helper".center(60,' ')
 print " "
 print "Type 'help' to get a list of commands. ".center(60,' ')
 while init == 1:
@@ -20,14 +24,29 @@ while init == 1:
         print "===="
         print " "
         print "still in progress"
-    elif cmd == "test":
-        phrase = raw_input('Enter phrase: ')
-        p =  re.sub('([^\s\w]|_)+', '',phrase).replace(' ','-')
-        d = str(datetime.date.today())
-        title = d + '-' + p
-        print "Expected blog post file: " + title + ".html.markdown"
+    elif cmd == "make article":
+        a_title = raw_input('Title: ')
+        re_phrase = re.sub('([^\s\w]|_)+', '',a_title).lower().replace(' ','-')
+        today = str(datetime.date.today())
+        filename = today + '-' + re_phrase + '.html.markdown'
+        if blog_dir == 1:
+            savedir = os.path.abspath(r'source\blog')
+        else:
+            savedir = os.path.abspath('source')
+        savefile = os.path.join(savedir, filename)
+        with open(savefile, "w+") as file:
+            file.write("---\n")
+            file.write("# File created using Middleman Helper")
+            file.write("title: " + a_title + "\n")
+            file.write("date: " + today + "\n")
+            file.write("tags: \n")
+            file.write("---\n")
+            file.write("\n")
+            file.close()
+        print 'Creating "%s"...' % filename
+        print 'Done.'
     elif cmd == "exit" or cmd == "x":
         die()
     else:
-        print "'%s' not found" % cmd
+        print "'%s' command not found" % cmd
     
